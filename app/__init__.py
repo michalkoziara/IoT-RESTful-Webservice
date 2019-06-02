c3c46086b55ac1fd3a64a3a68f35b52cf5abf4fc
Micha³ Koziara <mkoziara12@gmail.com>
2019-06-02 11:24:47 +0200

    Versioning.

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,9 @@ from .main.controller.user_controller import api as user_ns
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint,
-          title='FLASK RESTPLUS API BOILER-PLATE',
-          version='1.0',
-          description='a boilerplate for flask restplus web service'
+          title='API',
+          version='0.1',
+          description='a web service API'
           )
 
 api.add_namespace(user_ns, path='/user')
