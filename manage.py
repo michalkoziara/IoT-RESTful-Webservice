0f7ba10e6b97c1e027b9388ae42e87b972f3d17f
Michał Koziara <mkoziara12@gmail.com>
2019-08-17 19:24:51 +0100

    Add manager command for displaying routes
    It is useful for displaying currently available routes. It provides way
    for testing if routes were correctly defined.

--- manage.py
+++ manage.py
@@ -38,5 +38,23 @@ def test():
         return 0
     return 1
 
+@manager.command
+def routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.parse.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+    
+    for line in sorted(output):
+        print(line)
+
 if __name__ == '__main__':
     manager.run()
