77a826d3925e4b381aebb5cc9b07ff7641d58b46
Michał Koziara <mkoziara12@gmail.com>
2019-09-20 13:09:05 +0100

    Add model for user

--- app/main/model/user.py
+++ app/main/model/user.py
@@ -6,8 +6,8 @@ class User(db.Model):
     __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(255), unique=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
-    admin = db.Column(db.Boolean, nullable=False, default=False)
-    public_id = db.Column(db.String(100), unique=True)
-    username = db.Column(db.String(50), unique=True)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    password = db.Column(db.String(255), nullable=False)
