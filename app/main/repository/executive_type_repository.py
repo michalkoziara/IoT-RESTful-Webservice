063d5586432e292e4727b36683aececc0672b77c
Piotr Kramek <PeterKramek@gmail.com>
2019-10-11 13:24:45 +0200

    Add executive type repository

---
+++ app/main/repository/executive_type_repository.py
@@ -0,0 +1,18 @@
+from sqlalchemy import and_
+
+from app.main.model.executive_type import ExecutiveType
+
+
+class ExecutiveTypeRepository:
+    _instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+
+        return cls._instance
+
+    def get_executive_type_by_id(self, id: str) \
+            -> ExecutiveType:
+        return ExecutiveType.query.filter(ExecutiveType.id == id).first()
