51521f2a146f068e1b4e72e8431df9726e08c4c7
Piotr Kramek <PeterKramek@gmail.com>
2019-10-12 10:35:23 +0200

    Fix get_executive_device_info

--- app/main/service/executive_device_service.py
+++ app/main/service/executive_device_service.py
@@ -9,6 +9,7 @@ class ExecutiveDeviceService:
 
     _device_group_repository_instance = None
     _executive_device_repository = None
+    _executive_device_type_repository = None
     _formula_repository = None
 
     @classmethod
@@ -22,20 +23,23 @@ class ExecutiveDeviceService:
         self._device_group_repository_instance = DeviceGroupRepository.get_instance()
         self._executive_device_repository = ExecutiveDeviceRepository.get_instance()
         self._formula_repository = FormulaRepository.get_instance()
+        self._executive_device_type_repository = ExecutiveTypeRepository.get_instance()
 
     def get_executive_device_info(self, device_key: str, product_key: str):
         if device_key is None:
             return False, None
 
+        # TODO add more LOGIC!!
+
         device_group = self._device_group_repository_instance.get_device_group_by_product_key(product_key)
 
-        if device_group is None:
+        if not device_group:
             return False, None
 
         executive_device = self._executive_device_repository.get_executive_device_by_device_key_and_device_group_id(
             device_key, device_group.id)
 
-        if executive_device is None:
+        if not executive_device:
             return False, None
 
         executive_device_info = {}
@@ -47,12 +51,17 @@ class ExecutiveDeviceService:
         executive_device_info['positive_state'] = executive_device.positive_state
         executive_device_info['negative_state'] = executive_device.negative_state
         executive_device_info['device_key'] = executive_device.device_key
-        executive_device_info['deviceTypeName'] = ExecutiveTypeRepository.get_executive_type_by_id(
-            executive_device.executive_type_id).name
-        executive_device_info[
-            'deviceUserGroup'] = self._device_group_repository_instance.get_device_group_by_product_key(
-            product_key).name
-        executive_device_info['formulaName'] = self._formula_repository.get_formula_by_id(
-            executive_device.formula_id).name
+        executive_device_info['deviceUserGroup'] = device_group.name
+
+        executive_device_type = self._executive_device_type_repository.get_executive_type_by_id(
+            executive_device.executive_type_id)
+        executive_device_info['deviceTypeName'] = executive_device_type.name
+
+        formula = self._formula_repository.get_formula_by_id(
+            executive_device.formula_id)
+        if formula:
+            executive_device_info['formulaName'] = formula.name
+        else:
+            executive_device_info['formulaName'] = None
 
         return True, executive_device_info
