d8f25f9cc396b31ba47ff706baa88eaae48a9f9f
michalkoziara <mkoziara12@gmail.com>
2019-10-12 13:38:04 +0200

    Add get device by key to non-configured device rep

--- app/main/repository/unconfigured_device_repository.py
+++ app/main/repository/unconfigured_device_repository.py
@@ -21,6 +21,9 @@ class UnconfiguredDeviceRepository:
     def get_unconfigured_devices_by_device_group_id(self, device_group_id: str) -> List[UnconfiguredDevice]:
         return UnconfiguredDevice.query.filter(UnconfiguredDevice.device_group_id == device_group_id).all()
 
+    def get_unconfigured_device_by_device_key(self, device_key: str) -> UnconfiguredDevice:
+        return UnconfiguredDevice.query.filter(UnconfiguredDevice.device_key == device_key).first()
+
     def save(self, unconfigured_device: UnconfiguredDevice) -> bool:
         try:
             db.session.add(unconfigured_device)
