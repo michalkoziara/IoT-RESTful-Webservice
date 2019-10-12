7d9a51bf2562f38cbc3c87c7842858d209fcff0b
michalkoziara <mkoziara12@gmail.com>
2019-10-12 13:39:02 +0200

    Add create_device to hub controller

--- app/main/controller/hub_controller.py
+++ app/main/controller/hub_controller.py
@@ -1,13 +1,15 @@
 import datetime
 import json
+import traceback
 
 from flask import Response
+from flask import request
+from werkzeug.exceptions import BadRequest
 
 from app import api
 from app.main.service.log_service import LogService
 from app.main.service.hub_service import HubService
 from app.main.util.constants import Constants
-
 
 _hub_service_instance = HubService.get_instance()
 
@@ -16,7 +18,6 @@ _logger = LogService.get_instance()
 
 @api.route('/hubs/<product_key>/states', methods=['GET'])
 def get_states(product_key):
-
     result, result_values = \
         _hub_service_instance.get_changed_devices_for_device_group(product_key)
 
@@ -34,6 +35,80 @@ def get_states(product_key):
             ),
             product_key
         )
+
+    return Response(
+        response=json.dumps(response),
+        status=status,
+        mimetype='application/json')
+
+
+@api.route('/hubs/<product_key>/devices', methods=['POST'])
+def create_device(product_key: str):
+    response = None
+    status = None
+    request_dict = None
+    device_key = None
+
+    if not request.is_json:
+        response = dict(errorMessage=Constants.RESPONSE_MESSAGE_BAD_MIMETYPE)
+        status = 400
+        _logger.log_exception(
+            dict(
+                type='Error',
+                creationDate=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                errorMessage=response['errorMessage'],
+            ),
+            product_key
+        )
+    else:
+        try:
+            request_dict = request.get_json()
+
+            try:
+                device_key = request_dict['deviceKey']
+            except KeyError as e:
+                response = dict(errorMessage=Constants.RESPONSE_MESSAGE_BAD_REQUEST)
+                status = 400
+                _logger.log_exception(
+                    dict(
+                        type='Error',
+                        creationDate=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                        errorMessage=response['errorMessage'],
+                        stackTrace=traceback.format_exc(),
+                        payload=json.dumps(request_dict)
+                    ),
+                    product_key
+                )
+        except BadRequest as e:
+            response = dict(errorMessage=e.description)
+            status = e.code
+            _logger.log_exception(
+                dict(
+                    type='Error',
+                    creationDate=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                    errorMessage=response['errorMessage'],
+                    stackTrace=traceback.format_exc()
+                ),
+                product_key
+            )
+
+    if status is None:
+        result = _hub_service_instance.add_device_to_device_group(product_key, device_key)
+
+        if result is True:
+            status = 201
+        else:
+            response = dict(errorMessage=Constants.RESPONSE_MESSAGE_CONFLICTING_DATA)
+            status = 409
+            _logger.log_exception(
+                dict(
+                    type='Error',
+                    creationDate=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                    errorMessage=response['errorMessage'],
+                    payload=json.dumps(request_dict)
+                ),
+                product_key
+            )
 
     return Response(
         response=json.dumps(response),
