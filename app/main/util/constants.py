f7b5774414bbad02b4e9467cb5ad7b479241d69d
michalkoziara <mkoziara12@gmail.com>
2019-10-16 22:18:53 +0200

    Add constants for user management

--- app/main/util/constants.py
+++ app/main/util/constants.py
@@ -9,14 +9,19 @@ class Constants:
     DATABASE_URL_PROD = os.environ.get('DATABASE_URL')
     DATABASE_URL_TEST = 'sqlite:///' + os.path.join(
         os.path.abspath(os.path.dirname(__file__)), 'flask_boilerplate_test.db')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
+    RESPONSE_MESSAGE_ERROR = 'Server encountered an unexpected condition that prevented it from fulfilling the request.'
     RESPONSE_MESSAGE_BAD_REQUEST = 'The browser (or proxy) sent a request that this server could not understand.'
     RESPONSE_MESSAGE_CONFLICTING_DATA = 'The browser (or proxy) sent a request with conflicting data.'
     RESPONSE_MESSAGE_BAD_MIMETYPE = (
         'The browser (or proxy) sent a request with mimetype that does not indicate JSON data.')
-    RESPONSE_MESSAGE_PRODUCT_KEY_NOT_FOUND = 'Product key was not found'
-    RESPONSE_MESSAGE_DEVICE_KEY_NOT_FOUND = 'Device key was not found'
-    RESPONSE_MESSAGE_USER_DOES_NOT_HAVE_PRIVILEGES = 'User does not have necessary privileges'
-    RESPONSE_MESSAGE_USER_NOT_DEFINED = 'User not defined'
+    RESPONSE_MESSAGE_PRODUCT_KEY_NOT_FOUND = 'Product key was not found.'
+    RESPONSE_MESSAGE_DEVICE_KEY_NOT_FOUND = 'Device key was not found.'
+    RESPONSE_MESSAGE_USER_DOES_NOT_HAVE_PRIVILEGES = 'User does not have necessary privileges.'
+    RESPONSE_MESSAGE_INVALID_CREDENTIALS = 'User credentials are invalid.'
+    RESPONSE_MESSAGE_USER_NOT_DEFINED = 'User not defined.'
+    RESPONSE_MESSAGE_USER_ALREADY_EXISTS = 'User with given credentials already exists.'
+    RESPONSE_MESSAGE_SIGNATURE_EXPIRED = 'Signature expired.'
+    RESPONSE_MESSAGE_INVALID_TOKEN = 'Invalid token.'
     RESPONSE_MESSAGE_OK = 'OK'
