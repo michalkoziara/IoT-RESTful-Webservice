9119e1b392a307e7e3fbd325e7c253c764c13da7
michalkoziara <mkoziara12@gmail.com>
2019-10-17 22:09:17 +0200

    Add unit tests for registration

--- app/test/unittest/test_user.py
+++ app/test/unittest/test_user.py
@@ -29,7 +29,6 @@ def test_create_auth_token_should_return_auth_token_when_valid_user_credentials(
     assert result
     assert result == Constants.RESPONSE_MESSAGE_OK
     assert token
-    assert len(token) == 139
 
     payload = jwt.decode(token, Constants.SECRET_KEY)
     assert payload['sub'] == user.id
@@ -77,6 +76,77 @@ def test_create_auth_token_should_return_invalid_credentials_message_when_invali
     assert result
     assert result == Constants.RESPONSE_MESSAGE_INVALID_CREDENTIALS
     assert token is None
+
+
+def test_create_user_should_return_success_message_when_valid_parameters():
+    user_service_instance = UserService.get_instance()
+
+    with patch.object(UserRepository, 'get_user_by_email_or_username') as get_user_by_email_or_username_mock:
+        get_user_by_email_or_username_mock.return_value = None
+
+        with patch.object(UserRepository, 'save') as save_mock:
+            save_mock.return_value = True
+
+            result = user_service_instance.create_user('username', 'email', 'password')
+
+    assert result
+    assert result == Constants.RESPONSE_MESSAGE_OK
+
+
+@pytest.mark.parametrize("username, email, password", [
+    ('test username', 'test email', None),
+    ('test username', None, 'test password'),
+    (None, 'test email', 'test password')])
+def test_create_user_should_return_bad_request_message_when_no_parameter(username, email, password):
+    user_service_instance = UserService.get_instance()
+
+    result = user_service_instance.create_user(username, email, password)
+
+    assert result
+    assert result == Constants.RESPONSE_MESSAGE_BAD_REQUEST
+
+
+def test_create_user_should_return_user_already_exists_message_when_duplicate_username(create_user):
+    user_service_instance = UserService.get_instance()
+
+    user = create_user()
+
+    with patch.object(UserRepository, 'get_user_by_email_or_username') as get_user_by_email_or_username_mock:
+        get_user_by_email_or_username_mock.return_value = user
+
+        result = user_service_instance.create_user(user.username, 'test email', 'password')
+
+    assert result
+    assert result == Constants.RESPONSE_MESSAGE_USER_ALREADY_EXISTS
+
+
+def test_create_user_should_return_user_already_exists_message_when_duplicate_email(create_user):
+    user_service_instance = UserService.get_instance()
+
+    user = create_user()
+
+    with patch.object(UserRepository, 'get_user_by_email_or_username') as get_user_by_email_or_username_mock:
+        get_user_by_email_or_username_mock.return_value = user
+
+        result = user_service_instance.create_user('test username', user.email, 'password')
+
+    assert result
+    assert result == Constants.RESPONSE_MESSAGE_USER_ALREADY_EXISTS
+
+
+def test_create_user_should_return_error_message_when_save_failed():
+    user_service_instance = UserService.get_instance()
+
+    with patch.object(UserRepository, 'get_user_by_email_or_username') as get_user_by_email_or_username_mock:
+        get_user_by_email_or_username_mock.return_value = None
+
+        with patch.object(UserRepository, 'save') as save_mock:
+            save_mock.return_value = False
+
+            result = user_service_instance.create_user('username', 'email', 'password')
+
+    assert result
+    assert result == Constants.RESPONSE_MESSAGE_ERROR
 
 
 if __name__ == '__main__':
