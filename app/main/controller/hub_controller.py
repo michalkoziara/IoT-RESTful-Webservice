e1c53a180713c282e36d4d98bdfd86b9127b88eb
Piotr Kramek <PeterKramek@gmail.com>
2019-10-17 11:33:47 +0200

    Refactor set_sensors_readings_and_devices_states

--- app/main/controller/hub_controller.py
+++ app/main/controller/hub_controller.py
@@ -118,7 +118,7 @@ def create_device(product_key: str):
 @api.route('/hubs/<product_key>/states', methods=['POST'])
 def set_sensors_readings_and_devices_states(product_key):
     # TODO add hub authentication
-    response = None
+    result = None
     status = None
     request_dict = None
 
@@ -137,6 +137,19 @@ def set_sensors_readings_and_devices_states(product_key):
         try:
             request_dict = request.get_json()
 
+            if 'sensors' not in request_dict or 'devices' not in request_dict:
+                response = dict(errorMessage=Constants.RESPONSE_MESSAGE_BAD_REQUEST)
+                status = 400
+                _logger.log_exception(
+                    dict(
+                        type='Error',
+                        creationDate=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                        errorMessage=response['errorMessage'],
+                        payload=json.dumps(request_dict)
+                    ),
+                    product_key
+                )
+
         except BadRequest as e:
             response = dict(errorMessage=e.description)
             status = e.code
@@ -149,48 +162,37 @@ def set_sensors_readings_and_devices_states(product_key):
                 ),
                 product_key
             )
-        try:
-            sensors_readings = request_dict['sensors']
-            devices_states = request_dict['devices']
-        except KeyError as e:
-            response = dict(errorMessage=Constants.RESPONSE_MESSAGE_BAD_REQUEST)
-            status = 400
-            _logger.log_exception(
-                dict(
-                    type='Error',
-                    creationDate=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
-                    errorMessage=response['errorMessage'],
-                    stackTrace=traceback.format_exc(),
-                    payload=json.dumps(request_dict)
-                ),
-                product_key
-            )
 
     if status is None:
+        sensors_readings = request_dict['sensors']
+        devices_states = request_dict['devices']
+
         result = _hub_service_instance.set_devices_states_and_sensors_readings(
             product_key,
             sensors_readings,
             devices_states
         )
 
-    if result in [Constants.RESPONSE_MESSAGE_UPDATED_SENSORS_AND_DEVICES,
-                  Constants.RESPONSE_MESSAGE_PARTIALLY_WRONG_DATA]:
-        status = 201
+        if result in [Constants.RESPONSE_MESSAGE_UPDATED_SENSORS_AND_DEVICES,
+                      Constants.RESPONSE_MESSAGE_PARTIALLY_WRONG_DATA]:
+            status = 201
+            response = {"errorMessage": result}
 
-    else:
-        response = dict(errorMessage=result)
-        status = 400
-        _logger.log_exception(
-            dict(
-                type='Error',
-                creationDate=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
-                errorMessage=response['errorMessage'],
-                payload=json.dumps(request_dict)  # TODO make sure that there is enough space in DB
-            ),
-            product_key
-        )
+
+        else:
+            response = dict(errorMessage=result)
+            status = 400
+            _logger.log_exception(
+                dict(
+                    type='Error',
+                    creationDate=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                    errorMessage=response['errorMessage'],
+                    payload=json.dumps(request_dict)  # TODO make sure that there is enough space in DB
+                ),
+                product_key
+            )
 
     return Response(
-        response=result,
+        response=json.dumps(response),
         status=status,
         mimetype='application/json')
