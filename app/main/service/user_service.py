c0dcbe7b367ce1fdc9cfc73f9c856e0903af916e
michalkoziara <mkoziara12@gmail.com>
2019-10-18 20:36:14 +0200

    Adjust login to allow admin logging in

--- app/main/service/user_service.py
+++ app/main/service/user_service.py
@@ -7,12 +7,14 @@ import flask_bcrypt
 from app.main.model.user import User
 from app.main.util.auth_utils import Auth
 from app.main.util.constants import Constants
+from app.main.repository.admin_repository import AdminRepository
 from app.main.repository.user_repository import UserRepository
 
 
 class UserService:
     _instance = None
 
+    _admin_repository_instance = None
     _user_repository_instance = None
 
     @classmethod
@@ -23,13 +25,19 @@ class UserService:
         return cls._instance
 
     def __init__(self):
+        self._admin_repository_instance = AdminRepository.get_instance()
         self._user_repository_instance = UserRepository.get_instance()
 
     def create_auth_token(self, email: str, password: str) -> Tuple[str, Optional[str]]:
         user = self._user_repository_instance.get_user_by_email(email)
+        is_admin = False
 
         if user is None:
-            return Constants.RESPONSE_MESSAGE_INVALID_CREDENTIALS, None
+            user = self._admin_repository_instance.get_admin_by_email(email)
+            is_admin = True
+
+            if user is None:
+                return Constants.RESPONSE_MESSAGE_INVALID_CREDENTIALS, None
 
         try:
             is_password_correct = flask_bcrypt.check_password_hash(user.password, password)
@@ -39,7 +47,7 @@ class UserService:
         if not is_password_correct:
             return Constants.RESPONSE_MESSAGE_INVALID_CREDENTIALS, None
 
-        token = Auth.encode_auth_token(user.id, user.is_admin)
+        token = Auth.encode_auth_token(user.id, is_admin)
 
         return Constants.RESPONSE_MESSAGE_OK, token
 
@@ -58,7 +66,6 @@ class UserService:
             username=username,
             email=email,
             registered_on=datetime.utcnow(),
-            is_admin=False,
             password=password_hash
         )
 
