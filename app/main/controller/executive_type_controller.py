308d36104ccccaa6f716a06ab9b522535052a6b6
michalkoziara <mkoziara12@gmail.com>
2019-10-20 21:14:58 +0200

    Refactor controllers to use response factory

--- app/main/controller/executive_type_controller.py
+++ app/main/controller/executive_type_controller.py
@@ -1,14 +1,10 @@
-import datetime
-import json
-
-from flask import Response
 from flask import request
 
 from app import api
 from app.main.service.log_service import LogService
 from app.main.service.executive_type_service import ExecutiveTypeService
 from app.main.util.auth_utils import Auth
-from app.main.util.constants import Constants
+from app.main.util.response_utils import ResponseUtils
 
 _logger = LogService.get_instance()
 
@@ -31,24 +27,9 @@ def get_executive_type(product_key: str, type_name: str):
     else:
         result = error_message
 
-    if result == Constants.RESPONSE_MESSAGE_OK:
-        response = result_values
-        status = 200
-    else:
-        response = dict(errorMessage=result)
-        status = 400
-        request_dict = None
-        _logger.log_exception(
-            dict(
-                type='Error',
-                creationDate=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
-                errorMessage=response['errorMessage'],
-                payload=json.dumps(request_dict)
-            ),
-            product_key
-        )
-
-    return Response(
-        response=json.dumps(response),
-        status=status,
-        mimetype='application/json')
+    return ResponseUtils.create_response(
+        result=result,
+        result_values=result_values,
+        product_key=product_key,
+        is_logged=True
+    )
--- app/main/controller/sensor_type_controller.py
+++ app/main/controller/sensor_type_controller.py
@@ -1,15 +1,10 @@
-import datetime
-import json
-
-from flask import Response
 from flask import request
 
 from app import api
 from app.main.service.log_service import LogService
 from app.main.service.sensor_type_service import SensorTypeService
 from app.main.util.auth_utils import Auth
-from app.main.util.constants import Constants
-from app.main.util.response_message_codes import response_message_codes
+from app.main.util.response_utils import ResponseUtils
 
 _logger = LogService.get_instance()
 
@@ -32,24 +27,9 @@ def get_sensor_type(product_key: str, type_name: str):
     else:
         result = error_message
 
-    if result == Constants.RESPONSE_MESSAGE_OK:
-        response = result_values
-        status = response_message_codes[result]
-    else:
-        response = dict(errorMessage=result)
-        status = response_message_codes[result]
-        request_dict = None
-        _logger.log_exception(
-            dict(
-                type='Error',
-                creationDate=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
-                errorMessage=response['errorMessage'],
-                payload=json.dumps(request_dict)
-            ),
-            product_key
-        )
-
-    return Response(
-        response=json.dumps(response),
-        status=status,
-        mimetype='application/json')
+    return ResponseUtils.create_response(
+        result=result,
+        result_values=result_values,
+        product_key=product_key,
+        is_logged=True
+    )
