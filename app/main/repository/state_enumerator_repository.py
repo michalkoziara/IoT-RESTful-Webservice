bef39685274527725e257a1f537edbf678df5745
Piotr Kramek <PeterKramek@gmail.com>
2019-10-21 17:33:09 +0200

    Add type parsing method to executive device service

--- app/main/repository/state_enumerator_repository.py
+++ app/main/repository/state_enumerator_repository.py
@@ -1,6 +1,8 @@
 # pylint: disable=no-self-use
 from typing import List
 
+from sqlalchemy import and_
+
 from app.main.model.state_enumerator import StateEnumerator
 from app.main.repository.base_repository import BaseRepository
 
@@ -19,3 +21,11 @@ class StateEnumeratorRepository(BaseRepository):
         return StateEnumerator.query.filter(
             StateEnumerator.executive_type_id == executive_type_id
         ).all()
+
+    def get_state_enumerator_by_executive_type_id_and_number(
+            self, executive_type_id: str, number: int) -> StateEnumerator:
+        return StateEnumerator.query.filter(
+            and_(
+                StateEnumerator.executive_type_id == executive_type_id,
+                StateEnumerator.number == number
+            )).first()
