6b9132ba33b981865ecaee5c040b9d5044514822
Piotr Kramek <PeterKramek@gmail.com>
2019-10-24 16:51:18 +0200

    Add tests

--- app/test/unittest/test_user_group.py
+++ app/test/unittest/test_user_group.py
@@ -1,5 +1,7 @@
 from unittest.mock import patch
 
+import pytest
+
 from app.main.repository.device_group_repository import DeviceGroupRepository
 from app.main.repository.executive_device_repository import ExecutiveDeviceRepository
 from app.main.repository.formula_repository import FormulaRepository
@@ -10,6 +12,153 @@ from app.main.service.executive_device_service import ExecutiveDeviceService
 from app.main.service.sensor_service import SensorService
 from app.main.service.user_group_service import UserGroupService
 from app.main.util.constants import Constants
+
+
+def test_get_list_of_user_groups_should_return_list_of_user_groups_names_when_valid_request(
+        create_device_group,
+        create_user,
+        get_user_group_default_values,
+        create_user_group,
+):
+    user_group_service = UserGroupService.get_instance()
+    device_group = create_device_group()
+    user = create_user()
+
+    first_user_group_values = get_user_group_default_values()
+    second_user_group_values = get_user_group_default_values()
+    third_user_group_values = get_user_group_default_values()
+
+    first_user_group_values['name'] = 'first'
+    second_user_group_values['name'] = 'second'
+    third_user_group_values['name'] = 'third'
+
+    second_user_group_values['id'] += 1
+    third_user_group_values['id'] += 2
+
+    first_user_group = create_user_group(first_user_group_values)
+    second_user_group = create_user_group(second_user_group_values)
+    third_user_group = create_user_group(third_user_group_values)
+
+    expected_output_values = ['first', 'second', 'third']
+
+    with patch.object(
+            DeviceGroupRepository,
+            'get_device_group_by_product_key'
+    ) as get_device_group_by_product_key_mock:
+        get_device_group_by_product_key_mock.return_value = device_group
+
+        with patch.object(UserRepository,
+                          'get_user_by_id') as get_user_by_id_mock:
+            get_user_by_id_mock.return_value = user
+
+            with patch.object(UserGroupRepository,
+                              'get_user_groups_by_device_group_id'
+                              ) as get_user_groups_by_device_group_id_mock:
+                get_user_groups_by_device_group_id_mock.return_value = [first_user_group,
+                                                                        second_user_group,
+                                                                        third_user_group]
+                result, result_values = user_group_service.get_list_of_user_groups(
+                    device_group.product_key,
+                    user.id
+                )
+    assert result == Constants.RESPONSE_MESSAGE_OK
+    assert result_values == expected_output_values
+
+
+def test_get_list_of_user_groups_should_return_empty_list_when_valid_request_and_no_user_groups_in_device_group(
+        create_device_group,
+        create_user,
+        get_user_group_default_values,
+        create_user_group,
+):
+    user_group_service = UserGroupService.get_instance()
+    device_group = create_device_group()
+    user = create_user()
+
+    expected_output_values = []
+
+    with patch.object(
+            DeviceGroupRepository,
+            'get_device_group_by_product_key'
+    ) as get_device_group_by_product_key_mock:
+        get_device_group_by_product_key_mock.return_value = device_group
+
+        with patch.object(UserRepository,
+                          'get_user_by_id') as get_user_by_id_mock:
+            get_user_by_id_mock.return_value = user
+
+            with patch.object(UserGroupRepository,
+                              'get_user_groups_by_device_group_id'
+                              ) as get_user_groups_by_device_group_id_mock:
+                get_user_groups_by_device_group_id_mock.return_value = []
+                result, result_values = user_group_service.get_list_of_user_groups(
+                    device_group.product_key,
+                    user.id
+                )
+    assert result == Constants.RESPONSE_MESSAGE_OK
+    assert result_values == expected_output_values
+
+
+def test_get_list_of_user_groups_should_return_error_message_when_user_not_found(
+        create_device_group
+):
+    user_group_service = UserGroupService.get_instance()
+    device_group = create_device_group()
+
+    with patch.object(
+            DeviceGroupRepository,
+            'get_device_group_by_product_key'
+    ) as get_device_group_by_product_key_mock:
+        get_device_group_by_product_key_mock.return_value = device_group
+
+        with patch.object(UserRepository,
+                          'get_user_by_id') as get_user_by_id_mock:
+            get_user_by_id_mock.return_value = None
+
+            result, result_values = user_group_service.get_list_of_user_groups(
+                device_group.product_key,
+                'user.id'
+            )
+    assert result == Constants.RESPONSE_MESSAGE_USER_NOT_DEFINED
+    assert result_values is None
+
+
+def test_get_list_of_user_groups_should_return_error_message_when_device_group_not_found(
+        create_device_group
+):
+    user_group_service = UserGroupService.get_instance()
+
+    with patch.object(
+            DeviceGroupRepository,
+            'get_device_group_by_product_key'
+    ) as get_device_group_by_product_key_mock:
+        get_device_group_by_product_key_mock.return_value = None
+
+        result, result_values = user_group_service.get_list_of_user_groups(
+            'device_group.product_key',
+            'user.id'
+        )
+
+    assert result == Constants.RESPONSE_MESSAGE_PRODUCT_KEY_NOT_FOUND
+    assert result_values is None
+
+
+@pytest.mark.parametrize("product_key, user_id, expected_result", [
+    ('product_key', None, Constants.RESPONSE_MESSAGE_USER_NOT_DEFINED),
+    (None, 'user_id', Constants.RESPONSE_MESSAGE_PRODUCT_KEY_NOT_FOUND)
+])
+def test_get_list_of_user_groups_should_return_error_message_when_one_of_parameters_is_none(
+        product_key, user_id, expected_result
+):
+    user_group_service = UserGroupService.get_instance()
+
+    result, result_values = user_group_service.get_list_of_user_groups(
+        product_key,
+        user_id
+    )
+
+    assert result == expected_result
+    assert result_values is None
 
 
 def test_get_list_of_executive_devices_should_return_list_of_devices_infos_when_valid_request(
