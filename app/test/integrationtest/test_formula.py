c2f195fa168a2457b2f5a78e05c883a38c666a87
michalkoziara <mkoziara12@gmail.com>
2019-10-29 23:47:42 +0100

    Add integration tests for getting formulas

--- app/test/integrationtest/test_formula.py
+++ app/test/integrationtest/test_formula.py
@@ -63,7 +63,7 @@ def test_create_formula_should_add_new_formula_to_use_group_when_valid_request(
         }
     )
 
-    assert response is not None
+    assert response
     assert response.status_code == 201
 
     response_data = json.loads(response.data.decode())
@@ -111,7 +111,7 @@ def test_create_formula_should_return_error_message_when_invalid_request(
         }
     )
 
-    assert response is not None
+    assert response
     assert response.status_code == 400
 
     response_data = json.loads(response.data.decode())
@@ -178,10 +178,127 @@ def test_create_formula_should_return_invalid_formula_message_when_invalid_formu
         }
     )
 
-    assert response is not None
+    assert response
     assert response.status_code == 400
 
     response_data = json.loads(response.data.decode())
     assert response_data
     assert response_data['errorMessage']
     assert response_data['errorMessage'] == Constants.RESPONSE_MESSAGE_INVALID_FORMULA
+
+
+def test_get_formulas_should_return_formula_names_when_valid_request(
+        client,
+        insert_device_group,
+        get_user_default_values,
+        insert_user,
+        get_user_group_default_values,
+        insert_user_group,
+        insert_formula):
+    content_type = 'application/json'
+
+    device_group = insert_device_group()
+    user = insert_user()
+    formula = insert_formula()
+
+    user_group_values = get_user_group_default_values()
+    user_group_values['users'] = [user]
+    user_group_values['formulas'] = [formula]
+    user_group = insert_user_group(user_group_values)
+
+    response = client.get(
+        '/api/hubs/' + device_group.product_key + '/user-groups/' + user_group.name + '/formulas',
+        content_type=content_type,
+        headers={
+            'Authorization': 'Bearer ' + Auth.encode_auth_token(user.id, False)
+        }
+    )
+
+    assert response
+    assert response.status_code == 200
+
+    response_data = json.loads(response.data.decode())
+    assert response_data
+    assert 'names' in response_data
+    assert formula.name in response_data['names']
+
+
+def test_get_formulas_should_return_error_message_when_invalid_request(
+        client,
+        insert_device_group,
+        get_user_default_values,
+        insert_user,
+        get_user_group_default_values,
+        insert_user_group,
+        insert_formula):
+    content_type = 'application/json'
+
+    device_group = insert_device_group()
+    user = insert_user()
+
+    user_group_values = get_user_group_default_values()
+    user_group_values['users'] = [user]
+    user_group = insert_user_group(user_group_values)
+    insert_formula()
+
+    response = client.get(
+        '/api/hubs/' + device_group.product_key + '/user-groups/' + 'not' + user_group.name + '/formulas',
+        content_type=content_type,
+        headers={
+            'Authorization': 'Bearer ' + Auth.encode_auth_token(user.id, False)
+        }
+    )
+
+    assert response
+    assert response.status_code == 400
+
+    response_data = json.loads(response.data.decode())
+    assert response_data
+    assert 'errorMessage' in response_data
+    assert response_data['errorMessage'] == Constants.RESPONSE_MESSAGE_USER_GROUP_NAME_NOT_FOUND
+
+
+def test_get_formulas_should_return_error_message_when_user_not_authorized(client):
+    content_type = 'application/json'
+
+    product_key = 'test product key'
+    user_group_name = 'test user group name'
+
+    response = client.get(
+        '/api/hubs/' + product_key + '/user-groups/' + user_group_name + '/formulas',
+        content_type=content_type,
+    )
+
+    assert response
+    assert response.status_code == 400
+
+    response_data = json.loads(response.data.decode())
+    assert response_data
+    assert 'errorMessage' in response_data
+    assert response_data['errorMessage'] == Constants.RESPONSE_MESSAGE_USER_NOT_DEFINED
+
+
+def test_get_formulas_should_return_no_privileges_error_message_when_user_is_admin(
+        client, insert_admin):
+    content_type = 'application/json'
+
+    admin = insert_admin()
+
+    product_key = 'test product key'
+    user_group_name = 'test user group name'
+
+    response = client.get(
+        '/api/hubs/' + product_key + '/user-groups/' + user_group_name + '/formulas',
+        content_type=content_type,
+        headers={
+            'Authorization': 'Bearer ' + Auth.encode_auth_token(admin.id, True)
+        }
+    )
+
+    assert response
+    assert response.status_code == 403
+
+    response_data = json.loads(response.data.decode())
+    assert response_data
+    assert 'errorMessage' in response_data
+    assert response_data['errorMessage'] == Constants.RESPONSE_MESSAGE_USER_DOES_NOT_HAVE_PRIVILEGES
