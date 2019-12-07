2a116cb81f240c903cb381bfa273ddf08d570788
PKramek <37769346+PKramek@users.noreply.github.com>
2019-12-07 15:29:19 +0100

    Change get list of non configured devices endpoint

--- app/main/service/unconfigured_device_service.py
+++ app/main/service/unconfigured_device_service.py
@@ -27,18 +27,24 @@ class UnconfiguredDeviceService:
     def get_unconfigured_device_keys_for_device_group(
             self,
             product_key: str,
-            user_id: str) -> Tuple[str, Optional[List[str]]]:
+            admin_id: str,
+            is_admin: bool) -> Tuple[str, Optional[List[str]]]:
 
         if product_key is None:
             return Constants.RESPONSE_MESSAGE_PRODUCT_KEY_NOT_FOUND, None
 
-        device_group = self._device_group_repository_instance.get_device_group_by_user_id_and_product_key(
-            user_id,
+        if admin_id is None or is_admin is None:
+            return Constants.RESPONSE_MESSAGE_USER_NOT_DEFINED, None
+
+        device_group = self._device_group_repository_instance.get_device_group_by_product_key(
             product_key
         )
 
         if not device_group:
             return Constants.RESPONSE_MESSAGE_PRODUCT_KEY_NOT_FOUND, None
+
+        if is_admin is not True or device_group.admin_id != admin_id:
+            return Constants.RESPONSE_MESSAGE_USER_DOES_NOT_HAVE_PRIVILEGES, None
 
         unconfigured_devices = (self._unconfigured_device_repository_instance
                                 .get_unconfigured_devices_by_device_group_id(device_group.id))
